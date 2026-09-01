19507d7a837c5e7f22d7a812ff0ea9c5f33a40a2
Rijan Regmi <rijanregmi111@gmail.com>
2026-09-01 21:37:58 +0545

    update skills and cv

--- lib/data/skills.ts
+++ lib/data/skills.ts
@@ -7,13 +7,18 @@ export interface SkillItem {
 
 export const SKILLS: SkillItem[] = [
   { name: 'TypeScript', level: 'Advanced', iconSrc: '/assets/skills/typescript.svg' },
+  { name: 'JavaScript', level: 'Expert', iconSrc: '/assets/skills/javascript.svg' },
   { name: 'React', level: 'Advanced', iconSrc: '/assets/skills/react.svg' },
   { name: 'Node.js', level: 'Proficient', iconSrc: '/assets/skills/nodejs.svg' },
-  { name: 'Express.js', level: 'Advanced', iconSrc: '/assets/skills/express.svg' },
+  { name: 'Flutter', level: 'Advanced', iconSrc: '/assets/skills/flutter.svg' },
   { name: 'Python', level: 'Advanced', iconSrc: '/assets/skills/python.svg' },
+  { name: 'R Language', level: 'Proficient', iconSrc: '/assets/skills/r.svg' },
+  { name: 'Figma', level: 'Advanced', iconSrc: '/assets/skills/figma.svg' },
+  { name: 'Docker', level: 'Proficient', iconSrc: '/assets/skills/docker.svg' },
+  { name: 'Firebase', level: 'Advanced', iconSrc: '/assets/skills/firebase.svg' },
+  { name: 'MongoDB', level: 'Proficient', iconSrc: '/assets/skills/mongodb.svg' },
   { name: 'SQL, noSQL', level: 'Advanced', iconSrc: '/assets/skills/postgresql.svg' },
+  { name: 'Tailwind CSS', level: 'Expert', iconSrc: '/assets/skills/tailwindcss.svg' },
   { name: 'Java', level: 'Proficient', iconSrc: '/assets/skills/java.svg', whiteBg: true },
   { name: 'PHP', level: 'Basic', iconSrc: '/assets/skills/php.svg' },
-  { name: 'MongoDB', level: 'Proficient', iconSrc: '/assets/skills/mongodb.svg' },
-  { name: 'Tailwind CSS', level: 'Expert', iconSrc: '/assets/skills/tailwindcss.svg' },
 ];
